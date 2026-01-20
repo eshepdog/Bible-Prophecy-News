75367e8ec23bcf478cb2589b9649a08400138902
eshepdog <shepherd1326013@gmail.com>
2026-01-20 12:58:14 -0600

    Update login.js

--- login.js
+++ login.js
@@ -1,7 +1,16 @@
 const gameText = document..getElementByld('game-text2');
 const userInput = document.getElementByld('user-input2');
-const submitBtn = document.getElementByld('submit-btn2');
+const submitBtn = document.getElementByld('submit-btn2'); 
 function print(text) {
   gameText.innerText += text + "\n";
   gameText.scrollTop = gameText.scrollHeight;
 }
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+let currentStep = "askUsername";
+let username = "";
+submitBtn.addEventListener("click", async () => {
+  const input = userInput.value.trim();
+  userInput.value = "";
+  
